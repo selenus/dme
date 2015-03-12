3f4ad9ea798099976b6e7dc1b3e288bf3fd88af2
Tobias Gradl <tobias@tgradl.de>
2015-03-12 18:34:54 +0000

    286: Implement persistence layer
    Task-Url: https://home.tgradl.de/mantisbt/view.php?id=286

--- src/main/java/eu/dariah/de/minfba/schereg/controller/SchemaController.java
+++ src/main/java/eu/dariah/de/minfba/schereg/controller/SchemaController.java
@@ -1,5 +1,8 @@
 package eu.dariah.de.minfba.schereg.controller;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/schema")
 public class SchemaController {
+	@Autowired private SessionFactory sessionFactory;
+	
 	@RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
 	public String getHome(Model model) {
+		
+        
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+ 
+         
+       /* XmlSchema person = new Person("Steve", "Balmer");
+        session.save(person);
+ 
+        Employee employee = new Employee("James", "Gosling", "Marketing", new Date());
+        session.save(employee);
+ 
+        Owner owner = new Owner("Bill", "Gates", 300L, 20L);
+        session.save(owner);
+ */
+         
+        session.getTransaction().commit();
+        session.close();
+		
 		return "schema/home";
 	}
 }
