f0136a022a2125b1872561d5b90724025ad8e439
Tobias Gradl <tobias@tgradl.de>
2015-10-02 08:28:35 +0000

    327: Temporary grammars with user id prefix
    Task-Url: https://home.tgradl.de/mantisbt/view.php?id=327

--- src/main/java/eu/dariah/de/minfba/schereg/controller/editor/GrammarEditorController.java
+++ src/main/java/eu/dariah/de/minfba/schereg/controller/editor/GrammarEditorController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import de.dariah.aai.javasp.web.helper.AuthInfoHelper;
+import de.dariah.samlsp.model.pojo.AuthPojo;
 import de.unibamberg.minf.gtf.TransformationEngine;
 import de.unibamberg.minf.gtf.exception.GrammarGenerationException;
 import de.unibamberg.minf.gtf.transformation.CompiledTransformationFunction;
@@ -31,6 +32,7 @@ import eu.dariah.de.minfba.core.metamodel.function.DescriptionGrammarImpl;
 import eu.dariah.de.minfba.core.metamodel.function.GrammarContainer;
 import eu.dariah.de.minfba.core.metamodel.function.TransformationFunctionImpl;
 import eu.dariah.de.minfba.core.metamodel.function.interfaces.DescriptionGrammar;
+import eu.dariah.de.minfba.core.metamodel.function.interfaces.SerializableDescriptionGrammar;
 import eu.dariah.de.minfba.core.metamodel.function.interfaces.TransformationFunction;
 import eu.dariah.de.minfba.core.web.controller.BaseTranslationController;
 import eu.dariah.de.minfba.core.web.pojo.ModelActionPojo;
@@ -131,12 +133,13 @@ public class GrammarEditorController extends BaseScheregController {
 		if (gSave==null) {
 			gSave = grammar;
 		}
-		
-		DescriptionGrammar gTmp = this.getTemporaryGrammar(gSave.getId());
+
+		AuthPojo auth = authInfoHelper.getAuth(request);
+		DescriptionGrammar gTmp = this.getTemporaryGrammar(gSave.getId(), auth.getUserId());
 		grammarService.clearGrammar(gTmp);
 		
 		grammarService.clearGrammar(gSave);
-		grammarService.saveGrammar((DescriptionGrammarImpl)gSave, authInfoHelper.getAuth(request));
+		grammarService.saveGrammar((DescriptionGrammarImpl)gSave, auth);
 		return result;
 	}
 	
@@ -156,7 +159,7 @@ public class GrammarEditorController extends BaseScheregController {
 	}
 		
 	@RequestMapping(method = RequestMethod.POST, value = "/async/upload")
-	public @ResponseBody ModelActionPojo uploadGrammar(@PathVariable String grammarId, @RequestParam boolean combined, @RequestParam String lexerGrammar, @RequestParam String parserGrammar) {
+	public @ResponseBody ModelActionPojo uploadGrammar(@PathVariable String grammarId, @RequestParam boolean combined, @RequestParam String lexerGrammar, @RequestParam String parserGrammar, HttpServletRequest request) {
 		ModelActionPojo result = new ModelActionPojo(false);
 		if (parserGrammar==null || parserGrammar.trim().isEmpty()) {
 			result.addFieldError("grammarContainer_parserGrammar", "~Parser grammar cannot be empty!");
@@ -167,7 +170,7 @@ public class GrammarEditorController extends BaseScheregController {
 		
 		if (result.getErrorCount()==0) {
 			try {
-				DescriptionGrammar g = getTemporaryGrammar(grammarId);
+				DescriptionGrammar g = getTemporaryGrammar(grammarId, authInfoHelper.getUserId(request));
 				grammarService.clearGrammar(g);
 				
 				result.setPojo(grammarService.saveTemporaryGrammar(g, lexerGrammar, parserGrammar));
@@ -180,10 +183,10 @@ public class GrammarEditorController extends BaseScheregController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/async/parse")
-	public @ResponseBody ModelActionPojo parseGrammar(@PathVariable String grammarId) {
+	public @ResponseBody ModelActionPojo parseGrammar(@PathVariable String grammarId, HttpServletRequest request) {
 		ModelActionPojo result = new ModelActionPojo(false);
 		try {
-			DescriptionGrammar g = getTemporaryGrammar(grammarId);
+			DescriptionGrammar g = getTemporaryGrammar(grammarId, authInfoHelper.getUserId(request));
 			result.setPojo(grammarService.parseTemporaryGrammar(g));
 			result.setSuccess(true);
 		} catch (GrammarGenerationException e) {
@@ -198,10 +201,10 @@ public class GrammarEditorController extends BaseScheregController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/async/compile")
-	public @ResponseBody ModelActionPojo validateGrammar(@PathVariable String grammarId) {
+	public @ResponseBody ModelActionPojo validateGrammar(@PathVariable String grammarId, HttpServletRequest request) {
 		ModelActionPojo result = new ModelActionPojo(false);
 		try {
-			DescriptionGrammar g = getTemporaryGrammar(grammarId);
+			DescriptionGrammar g = getTemporaryGrammar(grammarId, authInfoHelper.getUserId(request));
 			result.setPojo(grammarService.compileTemporaryGrammar(g));
 			result.setSuccess(true);
 		} catch (Exception e) {
@@ -211,13 +214,13 @@ public class GrammarEditorController extends BaseScheregController {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/async/sandbox")
-	public @ResponseBody ModelActionPojo sandboxGrammar(@PathVariable String grammarId, @RequestParam String baseMethod) {
+	public @ResponseBody ModelActionPojo sandboxGrammar(@PathVariable String grammarId, @RequestParam String baseMethod, HttpServletRequest request) {
 		ModelActionPojo result = new ModelActionPojo(false);
 		try {
 			if (baseMethod==null || baseMethod.trim().isEmpty()) {
 				result.setSuccess(true);				
 			} else {
-				DescriptionGrammar g = getTemporaryGrammar(grammarId);
+				DescriptionGrammar g = getTemporaryGrammar(grammarId, authInfoHelper.getUserId(request));
 				List<String> parserRules = grammarService.getParserRules(g);
 				if (parserRules.contains(baseMethod.trim())) {
 					result.setSuccess(true);
@@ -237,13 +240,13 @@ public class GrammarEditorController extends BaseScheregController {
 	
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/async/parseSample")
-	public @ResponseBody ModelActionPojo parseSampleInput(@PathVariable String grammarId, @RequestParam String initRule, @RequestParam String sample, @RequestParam(defaultValue="true") Boolean temporary) {
+	public @ResponseBody ModelActionPojo parseSampleInput(@PathVariable String grammarId, @RequestParam String initRule, @RequestParam String sample, @RequestParam(defaultValue="true") Boolean temporary, HttpServletRequest request) {
 		ModelActionPojo result = new ModelActionPojo(false);
 		try {
 			DescriptionGrammar g;
 			
 			if (temporary) {
-				g = getTemporaryGrammar(grammarId);
+				g = getTemporaryGrammar(grammarId, authInfoHelper.getUserId(request));
 			} else {
 				g = grammarService.findById(grammarId);
 			}
@@ -272,10 +275,11 @@ public class GrammarEditorController extends BaseScheregController {
 		return result;
 	}
 	
-	private DescriptionGrammar getTemporaryGrammar(String id) {
-		DescriptionGrammar g = new DescriptionGrammarImpl();
+	private DescriptionGrammar getTemporaryGrammar(String id, String userId) {
+		SerializableDescriptionGrammar g = new DescriptionGrammarImpl();
 		g.setTemporary(true);
 		g.setId(id);
+		g.setUserId(userId);
 		g.setGrammarName(g.getIdentifier());
 		return g;
 	}
