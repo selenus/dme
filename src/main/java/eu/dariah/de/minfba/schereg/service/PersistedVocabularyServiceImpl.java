aaaf4e8dae21cd6fe0baba8a621cbfc37fdae048
Tobias Gradl <tobias@tgradl.de>
2016-05-19 14:43:23 +0200

    #487: Handle lists of values that can be accessed by transformation functions

--- src/main/java/eu/dariah/de/minfba/schereg/service/PersistedVocabularyServiceImpl.java
+++ src/main/java/eu/dariah/de/minfba/schereg/service/PersistedVocabularyServiceImpl.java
@@ -2,17 +2,26 @@ package eu.dariah.de.minfba.schereg.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import de.unibamberg.minf.gtf.vocabulary.VocabularyEngine;
 import eu.dariah.de.minfba.schereg.dao.interfaces.PersistedVocabularyDao;
 import eu.dariah.de.minfba.schereg.model.PersistedVocabulary;
 import eu.dariah.de.minfba.schereg.service.interfaces.PersistedVocabularyService;
 
 @Service
-public class PersistedVocabularyServiceImpl implements PersistedVocabularyService {
+public class PersistedVocabularyServiceImpl implements InitializingBean, PersistedVocabularyService {
 	@Autowired private PersistedVocabularyDao vocabularyDao;
+	@Autowired private VocabularyEngine vocabularyEngine;
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	@Override
 	public PersistedVocabulary findById(String id) {
 		return vocabularyDao.findById(id);
@@ -27,5 +36,4 @@ public class PersistedVocabularyServiceImpl implements PersistedVocabularyServic
 	public List<PersistedVocabulary> findAll() {
 		return vocabularyDao.findAll();
 	}
-
 }
