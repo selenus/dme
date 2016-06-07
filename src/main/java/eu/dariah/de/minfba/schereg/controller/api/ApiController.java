05e8d04b0a77ef2b7fabe0abf2fa4ca6cfc13e18
Tobias Gradl <tobias@tgradl.de>
2016-06-07 16:26:54 +0200

    #501: Implement simple and native schema API

---
+++ src/main/java/eu/dariah/de/minfba/schereg/controller/api/ApiController.java
@@ -0,0 +1,68 @@
+package eu.dariah.de.minfba.schereg.controller.api;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import de.dariah.aai.javasp.web.helper.AuthInfoHelper;
+import de.dariah.samlsp.model.pojo.AuthPojo;
+import eu.dariah.de.minfba.core.metamodel.interfaces.Element;
+import eu.dariah.de.minfba.core.metamodel.interfaces.Schema;
+import eu.dariah.de.minfba.core.metamodel.serialization.SerializableSchemaContainer;
+import eu.dariah.de.minfba.core.metamodel.xml.XmlSchema;
+import eu.dariah.de.minfba.schereg.model.RightsContainer;
+import eu.dariah.de.minfba.schereg.service.interfaces.ElementService;
+import eu.dariah.de.minfba.schereg.service.interfaces.SchemaService;
+
+@Controller
+@RequestMapping(value="/api/")
+public class ApiController {
+	protected static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+	
+	@Autowired protected AuthInfoHelper authInfoHelper;
+	@Autowired private SchemaService schemaService;
+	@Autowired protected ElementService elementService;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/schemas")
+	public @ResponseBody List<Schema> getSchemas(HttpServletRequest request) {
+		AuthPojo auth = authInfoHelper.getAuth(request);
+		List<RightsContainer<Schema>> schemas = schemaService.findAllByAuth(auth);
+		List<Schema> result = new ArrayList<Schema>();
+		if (schemas!=null) {
+			for (RightsContainer<Schema> s : schemas) {
+				if (s.getElement() instanceof XmlSchema) {
+					XmlSchema xmlS = ((XmlSchema)s.getElement());
+					xmlS.setTerminals(null);
+					xmlS.setNamespaces(null);
+					xmlS.setRootElementName(null);
+					xmlS.setRootElementNamespace(null);
+					xmlS.setRecordPath(null);
+				}
+				result.add(s.getElement());
+			}
+		}
+		return result;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/schemas/{entityId}")
+	public @ResponseBody SerializableSchemaContainer exportSchema(@PathVariable String entityId, HttpServletRequest request) {
+		AuthPojo auth = authInfoHelper.getAuth(request);
+		RightsContainer<Schema> s = schemaService.findByIdAndAuth(entityId, auth);
+		Element r = elementService.findRootBySchemaId(entityId, true);
+		
+		SerializableSchemaContainer sp = new SerializableSchemaContainer();
+		sp.setSchema(s.getElement());
+		sp.setRoot(r);
+		
+		return sp;
+	}
+}
