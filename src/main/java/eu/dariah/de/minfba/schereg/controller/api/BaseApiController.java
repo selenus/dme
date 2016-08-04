ca4dfad6a42464c77e7ba89e47cef194b11ba22b
Tobias Gradl <tobias@tgradl.de>
2016-08-04 10:57:14 +0200

    527: Consume grammars and functions with schema and mapping APIs and persist
    Task-Url: https://mantisbt.tgradl.de/view.php?id=527

--- src/main/java/eu/dariah/de/minfba/schereg/controller/api/BaseApiController.java
+++ src/main/java/eu/dariah/de/minfba/schereg/controller/api/BaseApiController.java
@@ -6,19 +6,27 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import eu.dariah.de.minfba.core.metamodel.function.DescriptionGrammarImpl;
+import eu.dariah.de.minfba.core.metamodel.function.GrammarContainer;
 import eu.dariah.de.minfba.core.metamodel.function.interfaces.DescriptionGrammar;
 import eu.dariah.de.minfba.schereg.service.interfaces.GrammarService;
 
 public abstract class BaseApiController {
 	@Autowired private GrammarService grammarService;
 	
-	protected Map<String, DescriptionGrammar> serializeGrammarSources(String entityId) {
-		Map<String, DescriptionGrammar> containers = new HashMap<String, DescriptionGrammar>();
+	protected Map<String, GrammarContainer> serializeGrammarSources(String entityId) {
+		Map<String, GrammarContainer> containers = new HashMap<String, GrammarContainer>();
 		List<DescriptionGrammar> grammars = grammarService.findByEntityId(entityId, true);
 
 		if (grammars!=null) {
 			for (DescriptionGrammar g : grammars) {
-				containers.put(g.getId(), g);
+				if (g.isPassthrough() || g.isError() || g.isTemporary()) {
+					continue;
+				}
+				
+				if (DescriptionGrammarImpl.class.isAssignableFrom(g.getClass())) {
+					containers.put(g.getId(), ((DescriptionGrammarImpl)g).getGrammarContainer());
+				}
 			}
 		}
 		return containers;
